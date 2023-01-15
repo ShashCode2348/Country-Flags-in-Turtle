8f2a98cd2c3448b3c1bbf0d79bdf9bc6ce16d601
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-01-15 13:44:56 +0000

    Update USA Flag.py

--- Flags/USA Flag.py
+++ Flags/USA Flag.py
@@ -14,64 +14,64 @@ def rectangle(horizontal, vertical, color):
     t.penup()
 
 def star(length, points, color):
-    sumangle = ((points * 2) - 2) * 180
+    sumangle = ((points*2)-2) * 180
     oneangle = sumangle/points
     smallangle = oneangle/3.5
     bigangle = oneangle - smallangle
     t.color(color)
-    t.begin_fill()
     t.pendown()
+    t.begin_fill()
+    t.penup()
     for counter in range(points):
         t.forward(length)
         t.left(smallangle)
         t.forward(length)
         t.left(bigangle)
     t.end_fill()
-    t.penup()
+
+t.penup()
 
 gotoy = 222
 
-#setup
 t.speed(0)
-t.setup(988, 520)
-t.penup()
+t.setup(988,520)
+t.goto(494,260)
+t.pendown()
 
-#red and white stripes
-t.goto(494, 260)
 for counter in range(7):
     t.setheading(-90)
-    rectangle(40, 988, '#B22234')
+    rectangle(40,988,'#B22234')
     t.setheading(-90)
     t.forward(80)
 
-#stars
+t.penup()
+
 t.setheading(0)
-t.goto(-494, 260)
-rectangle(494, 280, '#3C3B6E')
-t.goto(-474, 245)
-for counter in range(5):
+t.goto(-494,260)
+t.pendown()
+
+rectangle(494,280,'#3C3B6E')
+t.goto(-474,245)
+for counter in range(4):
     for counter in range(6):
-        star(10, 5, 'white')
+        star(9,5,'white')
         t.setheading(0)
         t.forward(84)
-    t.goto(-434, gotoy)
+        t.penup()
+    t.goto(-434,gotoy)
     gotoy = gotoy - 28
+    t.pendown()
     for counter in range(5):
-        star(10, 5, 'white')
+        star(9,5,'white')
         t.setheading(0)
         t.forward(84)
-    t.goto(-476, gotoy)
+    t.goto(-476,gotoy)
     gotoy = gotoy - 28
 
-#"Bye Bye Turtle!"
+for counter in range(6):
+        star(9,5,'white')
+        t.setheading(0)
+        t.forward(84)
+        t.penup()
+
 t.hideturtle()
-
-
-
-
-
-
-
-
-
-
